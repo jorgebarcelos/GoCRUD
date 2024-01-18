cadfd320506d539ab0fa882984b4034f61cf5587
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2024-01-18 15:36:44 -0300

    :category model, controller & view

--- src/controllers/categorycontroller/categorycontroller.go
+++ src/controllers/categorycontroller/categorycontroller.go
@@ -1,9 +1,11 @@
 package categorycontroller
 
 import (
+	"crud/src/entities"
 	"crud/src/models/categorymodel"
 	"html/template"
 	"net/http"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +23,30 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func Add(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmplt, err := template.ParseFiles("src/views/category/create.html")
 
+		if err != nil {
+			panic(err)
+		}
+
+		tmplt.Execute(w, nil)
+	}
+
+	if r.Method  == "POST" {
+		var category entities.Category
+
+		category.Name = r.FormValue("name")
+		category.CreatedAt = time.Now()
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Create(category); !ok {
+			tmplt, _ := template.ParseFiles("src/views/category/create.html")
+			tmplt.Execute(w, nil)
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
